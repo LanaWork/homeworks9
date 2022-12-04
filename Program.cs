93791a70efafe5f5868d131a7b7ecdc54175fa38
Lana <117645857+LanaWork@users.noreply.github.com>
2022-12-04 18:12:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+/*
+void PrintNumber(int N, int M)
+{
+  if (M > N) return;
+  PrintNumber(N, M + 1);
+  Console.Write(M + ", ");
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+int N = InputNumbers("Введите N: ");
+int M = 2;
+PrintNumber(N, M);
+Console.Write(1);
+  
+/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30*/
+/*
+void GapNumberSum (int M, int N, int sum)
+{
+    if (M > N) 
+    {
+        Console.WriteLine($"Сумма элементов от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (M++);
+    GapNumberSum(M, N, sum);
+}
+
+Console.WriteLine("Введите число M:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+ GapNumberSum(M, N, 0);
+*/
+
+
+/*Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29*/
+
+int AckermannFunction (int M, int N)
+{
+    if (M == 0) return N + 1;
+    if (M != 0 && N == 0) return AckermannFunction(M - 1, 1);
+    if (M > 0 && N > 0) return AckermannFunction(M - 1, AckermannFunction(M, N - 1));
+return AckermannFunction(M, N);
+}
+Console.WriteLine("Введите число M:");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Функция Аккермана для чисел ({M},{N}) = {AckermannFunction(M, N)}");
